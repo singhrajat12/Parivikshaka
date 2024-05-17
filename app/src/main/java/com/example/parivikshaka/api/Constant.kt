a75fc47911fa38992b75432d376ff191c7ba3073
Lenovo <singhrajatsingh1049@gmail.com>
2024-05-17 12:51:34 +0530

    Final Commit2

--- app/src/main/java/com/example/parivikshaka/api/Constant.kt
+++ app/src/main/java/com/example/parivikshaka/api/Constant.kt
@@ -5,7 +5,7 @@ class Constant {
 
 
 
-        const val AppUrl = "http://10.99.238.152:80/KKISANAPI/"
+        const val AppUrl = "https://10.99.238.152:80/KKISANAPI/"
         const val login = "api/v1/authenticate/user/"
 //        const val VERIFY_OTP = "api/v1/driver/verify-otp"
         const val PREFERENCE_NAME = "PREFERENCE_NAME"
